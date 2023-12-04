facda6fb4b2940c5c1bec7e31834d3d3bd11784d
Vitor Bonelli <bonelli5.vitor@gmail.com>
2023-12-04 20:24:58 -0300

    refactor: use view binding

--- app/src/main/java/com/vitor5bonelli/OneMessageChat/view/ConversasListActivity.kt
+++ app/src/main/java/com/vitor5bonelli/OneMessageChat/view/ConversasListActivity.kt
@@ -3,10 +3,14 @@ package com.vitor5bonelli.OneMessageChat.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.vitor5bonelli.OneMessageChat.R
+import com.vitor5bonelli.OneMessageChat.databinding.ActivityConversasListBinding
 
 class ConversasListActivity : AppCompatActivity() {
+
+    private lateinit var aclb: ActivityConversasListBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_conversas_list)
+        aclb = ActivityConversasListBinding.inflate(layoutInflater)
+        setContentView(aclb.root)
     }
 }
--- app/src/main/java/com/vitor5bonelli/OneMessageChat/view/LoginActivity.kt
+++ app/src/main/java/com/vitor5bonelli/OneMessageChat/view/LoginActivity.kt
@@ -3,10 +3,14 @@ package com.vitor5bonelli.OneMessageChat.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.vitor5bonelli.OneMessageChat.R
+import com.vitor5bonelli.OneMessageChat.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var alb: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        alb = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(alb.root)
     }
 }
