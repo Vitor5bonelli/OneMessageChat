c616e05e7c9b12104fbf40aa40fd2e7e414e3334
Vitor Bonelli <bonelli5.vitor@gmail.com>
2023-12-04 17:40:00 -0300

    chore: create mvc folders

--- app/src/main/java/com/vitor5bonelli/OneMessageChat/view/MainActivity.kt
+++ app/src/main/java/com/vitor5bonelli/OneMessageChat/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.vitor5bonelli.OneMessageChat
+package com.vitor5bonelli.OneMessageChat.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.vitor5bonelli.OneMessageChat.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
