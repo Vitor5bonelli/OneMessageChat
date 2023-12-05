3aec198d2df000713273b55cbae4ad27ad3d88b1
Vitor Bonelli <bonelli5.vitor@gmail.com>
2023-12-05 02:47:59 -0300

    style: finish chat card

--- app/src/main/java/com/vitor5bonelli/OneMessageChat/model/dao/UserDao.kt
+++
@@ -1,7 +0,0 @@
-package com.vitor5bonelli.OneMessageChat.model.dao
-
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
-
-class UserDao (){
-}
