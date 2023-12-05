61aee914b8fccf5bbb6992354da0d2d13f4a0552
Vitor Bonelli <bonelli5.vitor@gmail.com>
2023-12-04 21:27:50 -0300

    style: finish login xml

--- app/src/main/java/com/vitor5bonelli/OneMessageChat/view/ChatListActivity.kt
+++ app/src/main/java/com/vitor5bonelli/OneMessageChat/view/ChatListActivity.kt
@@ -12,4 +12,5 @@ class ChatListActivity : AppCompatActivity() {
         aclb = ActivityChatListBinding.inflate(layoutInflater)
         setContentView(aclb.root)
     }
+
 }
--- app/src/main/java/com/vitor5bonelli/OneMessageChat/view/LoginActivity.kt
+++ app/src/main/java/com/vitor5bonelli/OneMessageChat/view/LoginActivity.kt
@@ -33,4 +33,6 @@ class LoginActivity : AppCompatActivity() {
         }
     }
 
+    
+
 }
