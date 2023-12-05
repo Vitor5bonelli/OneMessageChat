67c658ec2930285b7f244ad6ab2af1debbc46b3b
Vitor Bonelli <bonelli5.vitor@gmail.com>
2023-12-04 21:42:50 -0300

    feat: implement login logic

--- app/src/main/java/com/vitor5bonelli/OneMessageChat/view/LoginActivity.kt
+++ app/src/main/java/com/vitor5bonelli/OneMessageChat/view/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.vitor5bonelli.OneMessageChat.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -22,6 +23,25 @@ class LoginActivity : AppCompatActivity() {
         auth = Firebase.auth
     }
 
+    override fun onResume() {
+        super.onResume()
+
+        //Go to register page
+        alb.orRegisterLoginTV.setOnClickListener{
+            switchToRegisterView()
+        }
+
+        //Try to login
+        alb.LoginBTN.setOnClickListener{
+            if ((alb.passwordLoginET.text.isBlank() || alb.passwordLoginET.text.isEmpty()) || (alb.emailLoginET.text.isBlank() || alb.emailLoginET.text.isEmpty()) ){
+                Toast.makeText(this, "Fields are empty!", Toast.LENGTH_LONG).show()
+            }
+            else{
+                signIn(alb.emailLoginET.text.toString(), alb.passwordLoginET.text.toString())
+            }
+        }
+    }
+
     private fun signIn(email: String, password: String){
         auth.signInWithEmailAndPassword(email, password).addOnCompleteListener { task ->
             if(task.isSuccessful){
@@ -33,6 +53,9 @@ class LoginActivity : AppCompatActivity() {
         }
     }
 
-    
+    private fun switchToRegisterView(){
+        val intent = Intent(this, RegisterActivity::class.java)
+        startActivity(intent)
+    }
 
 }
