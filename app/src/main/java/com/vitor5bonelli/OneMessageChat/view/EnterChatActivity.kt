796d1746c7bb4ff6f38f3d657e5568f013d507f5
Vitor Bonelli <bonelli5.vitor@gmail.com>
2023-12-05 22:28:30 -0300

    feat: add back button

--- app/src/main/java/com/vitor5bonelli/OneMessageChat/view/EnterChatActivity.kt
+++ app/src/main/java/com/vitor5bonelli/OneMessageChat/view/EnterChatActivity.kt
@@ -3,10 +3,21 @@ package com.vitor5bonelli.OneMessageChat.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.vitor5bonelli.OneMessageChat.R
+import com.vitor5bonelli.OneMessageChat.databinding.ActivityEnterChatBinding
 
 class EnterChatActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityEnterChatBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_enter_chat)
+        binding = ActivityEnterChatBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+    override fun onResume() {
+        super.onResume()
+
+        binding.backBTN.setOnClickListener{
+            finish()
+        }
     }
 }
